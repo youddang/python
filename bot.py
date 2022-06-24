acaf5ee42458144382d194cd92ec43f926ff8ca1
choi <choiyousung1020@gmail.com>
2022-06-24 21:03:00 +0900

    New File

--- bot.py
+++ bot.py
@@ -2,7 +2,21 @@ import discord
 import asyncio
 
 client = discord.Client()
-token = 'OTg5NTEzMzMxMzA2MDA0NTAw.GXwmq1.SX2kWfHmriDTpJbKkbqorBUSujQqbEBaMDlo0I'
+token = ''
 
 @client.event()
-async 
+async def on_message(msg):
+
+    print(f"{msg.channel} {msg.author} - {msg.content}")
+    if "탄압" in msg.content:
+        await msg.delete()
+        await msg.channel.send(f"{msg.author.mention} 님이 모함하셨습니다.")
+
+    if msg.content.startswith("$clear "):
+        purge_number = msg.content.replace("$clear ", "")
+        check_purge_number = purge_number.isdigit()
+
+        if check_purge_number == True:
+            await msg.channel.purge(limit = int(purge_number) + 1)
+        else:
+            await msg.channel.send("정수 값을 넣어주세요.")
